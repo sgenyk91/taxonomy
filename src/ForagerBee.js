6a1a54701abd99280586899a9bb33743f06f9a97
sgenyk91 <sgenyk91@gmail.com>
2014-11-03 15:41:02 -0800

    Finished bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.color = Bee.prototype.color;
+ForagerBee.prototype.food = Grub.prototype.food;
+ForagerBee.prototype.eat = Grub.prototype.eat;
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+// ForagerBee.prototype.color = 'red';
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.food = Grub.prototype.food;
+RetiredForagerBee.prototype.eat = Grub.prototype.eat;
+RetiredForagerBee.prototype.treasureChest = ForagerBee.prototype.treasureChest;
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
